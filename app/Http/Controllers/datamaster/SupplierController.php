54331b34f921c7d717f184041d10b03705f72046
FiTzZ6 <fikriramdan666@gmail.com>
2025-08-13 15:13:49 +0700

    TAMPILAN-SUPPLIER DAN BACKEND

---
+++ app/Http/Controllers/datamaster/SupplierController.php
@@ -0,0 +1,56 @@
+<?php
+
+namespace App\Http\Controllers\datamaster;
+
+
+use App\Models\Supplier;
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+
+class SupplierController extends Controller
+{
+    public function index()
+    {
+        $suppliers = Supplier::orderBy('kdsupplier')->get();
+        return view('datamaster.supplier', compact('suppliers'));
+    }
+
+    public function store(Request $request)
+    {
+        $validated = $request->validate([
+            'kdsupplier'   => 'required|string|max:25|unique:supplier,kdsupplier',
+            'namasupplier' => 'nullable|string|max:255',
+            'alamat'       => 'nullable|string',
+            'hp'           => 'nullable|string|max:15',
+            'email'        => 'nullable|email|max:255',
+            'ket'          => 'nullable|string',
+        ]);
+
+        Supplier::create($validated);
+        return response()->json(['success' => true]);
+    }
+
+    public function update(Request $request, $id)
+    {
+        $supplier = Supplier::findOrFail($id);
+
+        $validated = $request->validate([
+            'kdsupplier'   => 'required|string|max:25|unique:supplier,kdsupplier,' . $supplier->kdsupplier . ',kdsupplier',
+            'namasupplier' => 'nullable|string|max:255',
+            'alamat'       => 'nullable|string',
+            'hp'           => 'nullable|string|max:15',
+            'email'        => 'nullable|email|max:255',
+            'ket'          => 'nullable|string',
+        ]);
+
+        $supplier->update($validated);
+        return response()->json(['success' => true]);
+    }
+
+    public function destroy($id)
+    {
+        $supplier = Supplier::findOrFail($id);
+        $supplier->delete();
+        return response()->json(['success' => true]);
+    }
+}
--- resources/views/partials/navbar.blade.php
+++ resources/views/partials/navbar.blade.php
@@ -148,7 +148,7 @@
                     @if(session('typeuser') == 1)
                         <li><a href="{{ route('datamaster.area') }}">Area</a></li>
                         <li><a href="#">Cabang</a></li>
-                        <li><a href="#">Supplier</a></li>
+                        <li><a href="{{ route('datamaster.supplier') }}">Supplier</a></li>
                         <li><a href="#">Karyawan</a></li>
                         <li><a href="#">Kategori Barang</a></li>
                         <li><a href="#">Jenis Barang</a></li>
