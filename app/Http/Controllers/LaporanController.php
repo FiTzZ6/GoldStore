176a0d5c88dcf032b61cc7cbea12840142164553
FiTzZ6 <fikriramdan666@gmail.com>
2025-09-12 10:27:06 +0700

    revisi

--- app/Http/Controllers/LaporanController.php
+++ app/Http/Controllers/LaporanController.php
@@ -4,13 +4,13 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
-
+ 
 class LaporanController extends Controller
 {
     public function dashboard()
     {
         $company = DB::table('company_profile')->first();
-        return view('laporan.dashboard', compact('company'));
+        return view('dashboard', compact('company'));
     }
 
 }
